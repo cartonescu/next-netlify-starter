1bd3c76915408c3f21dee9fc6f1f1f186d09f13a
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-05-14 19:44:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,18 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Cartonescu.ro - Cumpara cutii de carton, folie cu bule si materiale de ambalare ieftine</title>
+    <meta name="description" content="Sc Amc Light Pack SRL (www.cartonescu.ro) - cumpara materiale pentru ambalarea usoara si rapida a obiectelor tale. Transporta in siguranta produsele tale folosind materialele noastre de ambalat." />
+    <meta name="robots" content="index, follow" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Cartonescu.ro" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>Cutii de carton ieftine, Cutii carton cu autoformare, cumpara folie cu bule ieftina, banda adeziva, folie stretch</code>
         </p>
+    <a href="https://amclightpack.ro">Sc Amc Light Pack SRL</a>
       </main>
 
       <Footer />
