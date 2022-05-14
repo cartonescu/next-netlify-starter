8ff77a99877109d88015f789cbb4e385a2789f71
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-05-14 23:24:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,27 @@ export default function Home() {
     <a href="https://amclightpack.ro">Sc Amc Light Pack SRL</a>
     <a href="https://www.cartonescu.ro">www.cartonescu.ro</a>
     
+    Compania AMC Light Pack SRL a fost concepută ca un magazin care activează în mediul online și este dedicată ambalejelor și produselor 
+    destinate pregătirii și împachetarii coletelor.
+
+Compania are o viziune de creștere și dezvoltare axată pe pasiunea pentru industrie, dar și pe grija față de clienți. 
+AMC Light Pack SRL își dorește să fie un partener stabil și de încredere pentru cei care îi aleg produsele și serviciile, de aceea pune accent pe 
+    câteva elemente de bază, punând la dispoziția consumatorilor produse de calitate dar accesibile, având prețuri competitive. 
+Viziune de dezvoltare permanentă, căutăm să înțelegem nevoile clienților noștri și să ne dezvoltăm firesc alături de consumatorii produselor noastre. 
+    Înființată într-un moment de creștere accelerată a nevoii de ambalaje, AMC Light Pack caută să se dezvolte permanent, să înțeleagă nevoile clienților 
+    și să îți dezvolte portofoliul de produse firesc, adaptându-se schimbărilor pieței. În această lume concurențială, care oferă soluții și produse de 
+    toate tipurile, amclightpack.ro caută să se diferențieze prin flexibilitatea și rapiditatea serviciilor, având o echipă de specialiști care poate 
+    consilia clienții, astfel încât să poată oferi produsele cele mai potrivite.
     
+Preferințele consumatorilor se îndreaptă acum către cumpărăturile online. Necesitatea de ambalaje este în creștere, așa că există o nevoie de produse 
+    care să ușureze munca și să facă procesele logistice mai simple, atât pentru companii cât și pentru utilizatorii casnici. AMC Light Pack oferă 
+    soluții de ambalare pentru firme dar și pentru persoane fizice, având în gamă produse din hârtie și carton, precum plicuri și cutii pentru depozitare 
+    și transport dar și soluții conexe de ambalare, cum ar fi folia stretch sau folia cu bule, plicuri pentru curierat si buzunare pentru AWB, benzi adezive 
+    sau accesorii de tăiere. 
+AMC Light Pack SRL este un furnizor local de produse de ambalare, capabil să susțină nevoile imediate ale clienților săi. Lucrând cu stocuri proprii, 
+    acesta poate să fie flexibil și să se adapteze oricărui tip de client, companie sau utilizator casnic. AMC Light Pack poate să livreze în întreaga 
+    țară în 24/48 de ore de la plasarea comenzii ferme, expedierea făcându-se din depozitul propriu din București. 
+
   
     
     
